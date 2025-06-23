ea7d7ccecef21f5effd655b6229b1515dfc65ebc
tianyongchao <13261590836@163.com>
2025-06-23 15:46:29 +0800

    Refactor ranking component layout and styles for improved UI consistency

--- src/views/ranking/index.vue
+++ src/views/ranking/index.vue
@@ -4,42 +4,43 @@
     <div class="banner">
       <img class="banner-bg" src="@/assets/img/ranking/header-countdown.png" alt="banner" />
     </div>
+
     <div class="ranking-content">
-    <!-- Tab/规则/奖励 -->
-    <div class="banner-content">
-      <div @click="showRule = true"><img class="icon-back" src="@/assets/img/ranking/text-rule.png" alt="back" /></div>
-      <div class="center-tab">
-        <div class="item-tab">明星榜</div>
-        <div class="active item-tab">荣耀榜</div>
-      </div>
-      <div><img class="icon-mic" src="@/assets/img/ranking/text-reward.png" alt="mic" @click="showReward = true" /></div>
-
-
-    </div>
-    <!-- 榜单头部 -->
-    <div class="list-header">
-      <div class="rank">排名</div>
-      <div class="user-info">用户信息</div>
-      <div class="contribution">贡献值</div>
-    </div>
-
-    <!-- 榜单内容 -->
-    <div class="ranking-list">
-      <div v-for="(item, idx) in rankingList" :key="item.id" class="ranking-item">
-        <div class="rank-num">
-          <img v-if="idx === 0" src="@/assets/img/ranking/1st.png" alt="1st" />
-          <img v-else-if="idx === 1" src="@/assets/img/ranking/2st.png" alt="2st" />
-          <img v-else-if="idx === 2" src="@/assets/img/ranking/3st.png" alt="3st" />
-          <span v-else>{{ (idx+1).toString().padStart(2, '0') }}</span>
+      <!-- Tab/规则/奖励 -->
+      <div class="banner-actions">
+        <img class="action-icon" src="@/assets/img/ranking/text-rule.png" alt="规则" @click="showRule = true" />
+        <div class="ranking-tabs">
+          <div class="tab-item">明星榜</div>
+          <div class="tab-item active">荣耀榜</div>
         </div>
-        <div class="user-info">
-          <div class="avatar"></div>
-          <span class="nickname">{{ item.nickname }}</span>
+        <img class="action-icon" src="@/assets/img/ranking/text-reward.png" alt="奖励" @click="showReward = true" />
+      </div>
+
+      <!-- 榜单头部 -->
+      <div class="list-header">
+        <div class="header-rank">排名</div>
+        <div class="header-user">用户信息</div>
+        <div class="header-score">贡献值</div>
+      </div>
+
+      <!-- 榜单内容 -->
+      <div class="ranking-list">
+        <div v-for="(item, idx) in rankingList" :key="item.id" class="ranking-item">
+          <div class="item-rank">
+            <img v-if="idx === 0" src="@/assets/img/ranking/1st.png" alt="1st" />
+            <img v-else-if="idx === 1" src="@/assets/img/ranking/2st.png" alt="2st" />
+            <img v-else-if="idx === 2" src="@/assets/img/ranking/3st.png" alt="3st" />
+            <span v-else>{{ (idx+1).toString().padStart(2, '0') }}</span>
+          </div>
+          <div class="item-user">
+            <div class="user-avatar"></div>
+            <span class="user-name">{{ item.nickname }}</span>
+          </div>
+          <div class="item-score">{{ item.score }}</div>
         </div>
-        <div class="score">{{ item.score }}</div>
       </div>
     </div>
-    </div>
+
     <RuleModal :visible="showRule" @close="showRule = false" />
     <RewardModal :visible="showReward" @close="showReward = false" />
   </div>
@@ -48,147 +49,157 @@
 <script>
 import RuleModal from './components/rule/index.vue';
 import RewardModal from './components/rule/RewardModal.vue';
+
 export default {
   components: { RuleModal, RewardModal },
   data() {
     return {
       showRule: false,
       showReward: false,
-      rankingList: [
-        { id: 1, nickname: '显示用户昵称', score: 123456 },
-        { id: 2, nickname: '显示用户昵称', score: 123456 },
-        { id: 3, nickname: '显示用户昵称', score: 123456 },
-        { id: 4, nickname: '显示用户昵称', score: 123456 },
-        { id: 5, nickname: '显示用户昵称', score: 123456 },
-      ],
+      rankingList: Array(5).fill().map((_, i) => ({
+        id: i + 1,
+        nickname: '显示用户昵称',
+        score: 123456
+      }))
     };
   },
 };
 </script>
 
 <style scoped lang="scss">
-.ranking-content {
-  position: relative;
-  padding-bottom: 30px;
-}
 .ranking-page {
   background: linear-gradient(90deg, #a36fff, #f7b3ff);
   min-height: 100vh;
   padding-bottom: 20px;
 }
+
 .banner {
-  position: relative;
-  width: 100%;
   height: 628px;
-  .banner-bg {
+
+  &-bg {
     width: 100%;
     height: 100%;
     object-fit: cover;
     border-radius: 0 0 30px 30px;
   }
 }
+
+.ranking-content {
+  padding: 20px 0 30px;
+}
+
+.banner-actions {
+  display: flex;
+  align-items: center;
+  gap: 41px;
+  margin-bottom: 20px;
+
+  .action-icon {
+    width: 124px;
+    height: 74px;
+    cursor: pointer;
+  }
+}
+
+.ranking-tabs {
+  flex: 1;
+  display: flex;
+  background: #fff;
+  border-radius: 37px;
+  height: 75px;
+  padding: 2px;
+
+  .tab-item {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #D5A8EF;
+    font-size: 28px;
+    min-width: 180px;
+
+    &.active {
+      color: #fff;
+      background: url('../../assets/img/ranking/tab-active.png') no-repeat;
+      background-size: 100% 100%;
+      padding: 22px;
+    }
+  }
+}
+
 .list-header {
   display: flex;
   justify-content: space-between;
   padding: 10px 16px;
   color: #fff;
   font-size: 20px;
-  margin-bottom: 8px;
-  .rank {
-    text-align: center;
+
+  .header-rank {
     width: 124px;
+    text-align: center;
   }
-  .user-info {
+
+  .header-user {
     flex: 1;
     padding-left: 20px;
   }
-  .contribution {
+
+  .header-score {
     padding-right: 40px;
   }
 }
+
 .ranking-list {
   padding: 0 10px;
-  .ranking-item {
-    height: 145px;
-    background-image: url('../../assets/img/ranking/item-bg.png');
-    background-size: cover;
-    background-repeat: no-repeat;
-    margin-bottom: 14px;
-    padding: 10px 16px;
+}
+
+.ranking-item {
+  display: flex;
+  align-items: center;
+  height: 145px;
+  padding: 10px 16px;
+  margin-bottom: 14px;
+  background: url('../../assets/img/ranking/item-bg.png') no-repeat;
+  background-size: cover;
+
+  .item-rank {
+    width: 84px;
+    text-align: center;
+    font-size: 44px;
+    font-weight: bold;
+    color: #fff;
+
+    img {
+      width: 84px;
+      height: 84px;
+    }
+  }
+
+  .item-user {
+    flex: 1;
     display: flex;
     align-items: center;
-    .rank-num {
-      width: 84px;
-      text-align: center;
-      font-size: 44px;
-      font-weight: bold;
-      color: #fff;
-      img {
-        width: 84px;
-        height: 84px;
-      }
+
+    .user-avatar {
+      width: 100px;
+      height: 100px;
+      margin: 0 10px 0 21px;
+      border-radius: 50%;
+      background: linear-gradient(0deg, #9F56CB 0%, #F988E7 100%);
     }
-    .user-info {
-      display: flex;
-      align-items: center;
-      flex: 1;
-      .avatar {
-        margin-left: 21px;
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
-        background: linear-gradient(0deg, #9F56CB 0%, #F988E7 100%);
-        margin-right: 10px;
-      }
-      .nickname {
-        color: #C170F0;
-        line-height: 50px;
-      }
+
+    .user-name {
+      color: #C170F0;
+      line-height: 50px;
     }
-    .score {
-      min-width: 60px;
-      text-align: right;
-      font-size: 30px;
-      color: #fff;
-      font-weight: bold;
-    }
+  }
+
+  .item-score {
+    min-width: 60px;
+    font-size: 30px;
+    font-weight: bold;
+    color: #fff;
+    text-align: right;
   }
 }
-.banner-content {
-  margin-top: 20px;
-  display: flex;
-  gap: 41px;
-  align-items: center;
-  .center-tab {
-    padding: 2px;
-    display: flex;
-    align-items: center;
-    color: #D5A8EF;
-    font-size: 28px;
-    border-radius: 37px;
-    height: 75px;
-    background-color: #fff;
-    flex: 1;
-    justify-content: space-between;
-    .item-tab {
-      text-align: center;
-      min-width: 180px !important;
-    }
-    .active {
-      padding: 22px;
-      background-image: url('../../assets/img/ranking/tab-active.png');
-      color: #fff;
-      background-size: 100% 100%;
-      background-repeat: no-repeat;
-    }
-  }
-    .icon-back {
-      width: 124px;
-      height: 74px;
-    }
-    .icon-mic {
-      width: 124px;
-      height: 74px;
-    }
-  }
 </style>
